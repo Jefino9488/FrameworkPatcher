d9f7166a1ad97b26a033121062c259d799f553d9
Jefino <jefinojacob9488@gmail.com>
2024-06-22 22:43:25 +0530

    add stuff

--- framework_patch.py
+++ framework_patch.py
@@ -20,8 +20,8 @@ def modify_file(file_path):
         "checkCapability": re.compile(r'\.method.*checkCapability\(.*\)Z'),
         "checkCapabilityRecover": re.compile(r'\.method.*checkCapabilityRecover\(.*\)Z'),
         "hasAncestorOrSelf": re.compile(r'\.method.*hasAncestorOrSelf\(.*\)Z'),
-        "getMinimumSignatureSchemeVersionForTargetSdk": re.compile(
-            r'\.method.*getMinimumSignatureSchemeVersionForTargetSdk\(I\)I')
+        "getMinimumSignatureSchemeVersionForTargetSdk": re.compile(r'\.method.*getMinimumSignatureSchemeVersionForTargetSdk\(I\)I'),
+        "collectCertificates": re.compile(r'\.method private static greylist-max-r collectCertificates\(Landroid/content/pm/PackageParser\$Package;Ljava/io/File;Z\)V')
     }
 
     for line in lines:
@@ -54,6 +54,14 @@ def modify_file(file_path):
                     modified_lines.append("    .registers 2\n")
                     modified_lines.append("    const/4 v0, 0x0\n")
                     modified_lines.append("    return v0\n")
+                elif method_type == "collectCertificates":
+                    logging.info(f"Modifying method body for {method_type}")
+                    in_body = False
+                    for body_line in lines:
+                        if not in_body and re.search(r'invoke-static \{v2, v0, v1\}, Landroid/util/apk/ApkSignatureVerifier;->unsafeGetCertsWithoutVerification\(Landroid/content/pm/parsing/result/ParseInput;Ljava/lang/String;I\)Landroid/content/pm/parsing/result/ParseResult;', body_line):
+                            modified_lines.append("    const/4 v1, 0x1\n")
+                            in_body = True
+                        modified_lines.append(body_line)
                 in_method = False
                 method_type = None
             else:
@@ -74,12 +82,30 @@ def modify_file(file_path):
     logging.info(f"Completed modification for file: {file_path}")
 
 
+def modify_exception_file(file_path):
+    logging.info(f"Modifying exception file: {file_path}")
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    modified_lines = []
+    for line in lines:
+        if re.search(r'iput p1, p0, Landroid/content/pm/PackageParser\$PackageParserException;->error:I', line):
+            logging.info(f"Adding line above 'iput' in {file_path}")
+            modified_lines.append("    const/4 p1, 0x0\n")
+        modified_lines.append(line)
+
+    with open(file_path, 'w') as file:
+        file.writelines(modified_lines)
+    logging.info(f"Completed modification for file: {file_path}")
+
+
 def modify_smali_files(directories):
     for directory in directories:
         signing_details = os.path.join(directory, 'android/content/pm/SigningDetails.smali')
-        package_parser_signing_details = os.path.join(directory,
-                                                      'android/content/pm/PackageParser$SigningDetails.smali')
+        package_parser_signing_details = os.path.join(directory, 'android/content/pm/PackageParser$SigningDetails.smali')
         apk_signature_verifier = os.path.join(directory, 'android/util/apk/ApkSignatureVerifier.smali')
+        package_parser = os.path.join(directory, 'android/content/pm/PackageParser.smali')
+        package_parser_exception = os.path.join(directory, 'android/content/pm/PackageParser$PackageParserException.smali')
 
         if os.path.exists(signing_details):
             logging.info(f"Found file: {signing_details}")
@@ -96,6 +122,16 @@ def modify_smali_files(directories):
             modify_file(apk_signature_verifier)
         else:
             logging.warning(f"File not found: {apk_signature_verifier}")
+        if os.path.exists(package_parser):
+            logging.info(f"Found file: {package_parser}")
+            modify_file(package_parser)
+        else:
+            logging.warning(f"File not found: {package_parser}")
+        if os.path.exists(package_parser_exception):
+            logging.info(f"Found file: {package_parser_exception}")
+            modify_exception_file(package_parser_exception)
+        else:
+            logging.warning(f"File not found: {package_parser_exception}")
 
 
 if __name__ == "__main__":
