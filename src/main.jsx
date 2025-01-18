b7e2eafe56d2ac723bc99214646506f913cd813f
jefino9488 <jefinojacob9488@gmail.com>
2025-01-18 08:10:09 +0530

    on update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-// import Maintenance from '@/components/maintenance.jsx'
+// import App from './App.jsx'
+import Maintenance from '@/components/maintenance.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    {/*<Maintenance/>*/}
+    {/*<App />*/}
+    <Maintenance/>
   </React.StrictMode>,
 )
